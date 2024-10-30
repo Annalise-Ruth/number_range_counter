1d9d55bec4057018f32732c6e7e0bfb983ba6e2d
Annie <annaliseruth.delacruz@gmail.com>
2024-10-30 18:41:07 +0800

    The loop was made and the ranges

--- number_range_counter.py
+++ number_range_counter.py
@@ -10,3 +10,26 @@ numbers_21_30 = 0
 numbers_31_40 = 0
 numbers_41_50 = 0
 
+while True:
+    try:
+        number = int(input("Please enter a number between 1 and 50: "))
+
+        if 1 <= number <= 10:
+            numbers_1_10 += 1
+        elif 11 <= number <= 20:
+            numbers_11_20 += 1
+        elif 21 <= number <= 30:
+            numbers_21_30 += 1
+        elif 31 <= number <= 40:
+            numbers_31_40 += 1
+        elif 41 <= number <= 50:
+            numbers_41_50 += 1
+        else:
+            print("Invalid input. Heading to Input Summary")
+            break
+    except ValueError:
+        print("Invalid input. Heading to Input Summary")
+        break
+
+
+   
